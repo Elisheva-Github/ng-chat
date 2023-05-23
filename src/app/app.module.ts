8ac6353e9a383b2c8ff321a7ce68bf10c07ea507
Your Name <you@example.com>
2023-05-23 16:41:28 +0300

    added service and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAUDP5IyJy65m4PmBUj9qc0tvF7HxVC0H0",
+        authDomain: "ng-chat-38c0a.firebaseapp.com",
+        projectId: "ng-chat-38c0a",
+        storageBucket: "ng-chat-38c0a.appspot.com",
+        messagingSenderId: "818899206356",
+        appId: "1:818899206356:web:d4c414bb6f59de856a7b41",
+        measurementId: "G-5H1XDK1L2K"
+    }
+};
